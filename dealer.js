28216c12f156fa1acf9ae980ead1047d21db2be1
Cloudyky <ikhwanch131@gmail.com>
2025-05-01 22:15:43 +0800

    new update

--- dealer.js
+++ dealer.js
@@ -1,11 +1,29 @@
 var brandlist = new Array("Porsche","Volkswagen","Audi","BMW");
-function newClient(){
-	var preference = Math.floor((Math.random()*4));
-	var time = Math.floor((Math.random()*10000)+1);
-	var client = Math.floor((Math.random()*10)+1);
-	$("#clients_queue").append('<div class="client client_'+client+'"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
-	setTimeout(function(){newClient();},time);
+
+function newClient() {
+	var preference = Math.floor((Math.random() * 4));
+	var time = Math.floor((Math.random() * 10000) + 1);
+	var client = Math.floor((Math.random() * 10) + 1);
+
+	// limit kan client max 10 je 
+	if ($(".client-queue-list .client").length < 10) {
+		$(".client-queue-list").append(
+			`
+				<div class="client client-${client}" id="client-${client}">
+					<div class="client-name">Customer for ${brandlist[preference]}</div>
+					<div class="client-img">
+						<img src="images/client_${client}.jpg" alt="client-${client}" srcset="">
+					</div>
+				</div>
+			`
+		);
+
+		$("#client-queue-totals").text($(".client-queue-list .client").length);
+	}
+
+	setTimeout(function () { newClient(); }, time);
 }
+
 $("document").ready(function(e) {
 	newClient();
 });
